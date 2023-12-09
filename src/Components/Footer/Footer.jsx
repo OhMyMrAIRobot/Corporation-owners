44efa5a294925e9f8add5017f0647634beb42a19
CallMeBaby <gfhieemobilelegends@gmail.com>
2023-12-09 21:38:36 +0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
-import Container from "@mui/material/Container";
 import "./Footer.css"
 import {Box} from "@mui/material";
 import AppBar from "@mui/material/AppBar";
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
+
+    const AboutLink = () => {
+        return (
+            <Link
+                className = 'AboutLink'
+                to="/About"
+                onClick={() => {
+                    window.scrollTo(0, 0);
+                }}
+            >
+                {t('appAbout')}
+            </Link>
+        )
+    }
+
+    const PrivacyLink = () => {
+        return (
+            <a
+                href = 'https://github.com/'
+                className = 'AboutLink'
+                target={"_blank"}
+            >
+                {t('policy')}
+            </a>
+        )
+    }
+
+
+    const { t } = useTranslation();
     return (
         <Box>
-            <AppBar position = "static">
-                ////
+            <AppBar
+                position = "static"
+                sx = {{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    minHeight: '50px',
+                    alignItems: 'center',
+                    fontSize: '1.2rem',
+                    whiteSpace: 'pre',
+                }}
+            >
+                © 2023 {t('bsuir')} • <AboutLink /> • <PrivacyLink />
             </AppBar>
         </Box>
     );

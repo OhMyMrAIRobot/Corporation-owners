599e89c2c62e187172e67ec8204ad8a529f37690
MrAIRobot <gfhieemobilelegends@gmail.com>
2023-12-14 15:04:43 +0300

    fixes

--- src/Components/PersonGrid/PersonGrid.jsx
+++ src/Components/PersonGrid/PersonGrid.jsx
@@ -4,12 +4,14 @@ import './PersonGrid.css'
 import MainContainer from "../MainContainer";
 import {Box, Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import Delimiter from "../Delimiter";
 
 
 const Item = ({person}) => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     return(
+
         <Card
             className = "cardPerson"
             sx = {{
@@ -54,11 +56,32 @@ export default function PersonGrid() {
     const persons = t("persons", { returnObjects: true });
     return (
         <MainContainer>
+
+            <Typography
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+                sx={{
+                    fontStyle: 'oblique',
+                    fontWeight: 'medium',
+                    textAlign: 'center',
+                    marginTop: '-40px',
+                }}
+            >
+                {t("appName")}
+            </Typography>
+
+            <Delimiter marginTop = '10px' paddingBottom = '40px'/>
+
             <Box className = "BoxItems">
                 {persons.map((item, key) =>(
                     <Item person={persons[key]}/>
                 ))}
             </Box>
+
+            <Delimiter marginTop = '10px'/>
+
         </MainContainer>
     );
 }
